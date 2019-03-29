4b6ad72e153ee6b7a1d809a6ab95d65499106d04
Juan Polanco <juanpolanco5008@gmail.com>
2019-03-29 12:24:17 -0400

    changed somethings around !

---
+++ main.js
@@ -0,0 +1,10 @@
+// making a function for document.query.selector and 
+//document.query.selector.all
+function query(selector) {
+    return document.query.selector(selector)
+}
+function queryAll (selector) {
+    return document.querySelectorAll(selector)
+}
+ 
+const 
